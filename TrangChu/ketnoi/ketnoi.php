b017c13fe0184e02f6ed6d0b71d8983d7692ebf9
levandat123 <levandat1405@gmail.com>
2024-07-27 00:52:07 +0700

    sua file ket noi, them bien moi truong cho file ket noi

--- TrangChu/ketnoi/ketnoi.php
+++ TrangChu/ketnoi/ketnoi.php
@@ -1,11 +1,16 @@
 <?php
-	$dbHost = 'mysql.railway.internal';
-	$dbUser = 'root';
-	$dbPassword = 'iGyDcTZVYGaufsyFwAlyxyObqYzRvxoM';
-	$dbName="railway";
-	$dbPort = 3306;
+	require __DIR__ . '/.../vendor/autoload.php';
 
-	$conn = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName,$dbPort);
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/...');
+	$dotenv->load();
+
+	$db_host = $_ENV['DB_HOST'];
+	$db_User = $_ENV['DB_USER'];
+	$db_port = $_ENV['DB_PORT'];
+	$db_database = $_ENV['DB_DATABASE'];
+	$db_password = $_ENV['DB_PASSWORD'];
+
+	$conn = mysqli_connect($db_host,$db_User,$db_port,$db_database,$db_password);
 
 	if($conn)
 	{
