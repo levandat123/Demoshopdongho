f3f6f2a3dba1a22dc0dd9cdf5913ab2a3f1914c6
levandat123 <levandat1405@gmail.com>
2024-07-27 19:21:46 +0700

    edit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit985ba293e635400363590a523e6a76ce
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit985ba293e635400363590a523e6a76ce', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit985ba293e635400363590a523e6a76ce', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '1128337e57e2625e9430c24ac6b985ec1f7e36ad',
+        'reference' => '4555ff662c2666bdd3379e45732c11308bb3392b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '1128337e57e2625e9430c24ac6b985ec1f7e36ad',
+            'reference' => '4555ff662c2666bdd3379e45732c11308bb3392b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
